e3da950aa8da01e3572af226901b31020c20d3e8
Jason Ji <jimanproductions@gmail.com>
2024-01-15 13:38:05 +0100

    adds new word

--- animations.js
+++ animations.js
@@ -5,11 +5,18 @@ document.getElementById('wordForm').addEventListener('submit', function(event) {
     addWordToCloud(word);
 });
 
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return `#${randomColor}`;
+}
+
+
 function addWordToCloud(word) {
     // This function would add the word to the visual word cloud
     // For the purposes of this example, we'll just append it to the div
     var wordCloud = document.getElementById('wordCloudBoard');
     var newWordSpan = document.createElement('span');
+    newWordSpan.style.color = getRandomColor();
     newWordSpan.textContent = word + " "; // Add space after word
     wordCloud.appendChild(newWordSpan);
   }
