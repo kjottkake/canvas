1efb9f68a2fd9b5dbcaca5d27c1677679137a30c
Jason Ji <jimanproductions@gmail.com>
2024-01-23 11:29:26 +0100

    added translation

--- animations.js
+++ animations.js
@@ -1,9 +1,32 @@
-document.getElementById('wordForm').addEventListener('submit', function(event) {
+document.getElementById('wordForm').addEventListener('submit', async function(event) {
     event.preventDefault();
     const word = document.getElementById('wordInput').value;
+
+    try {
+        const response = await fetch("http://localhost:5000/translate", {
+            method: "POST",
+            body: JSON.stringify({
+                q: word,
+                source: "nb",
+                target: "en",
+                format: "text",
+                api_key: ""
+            }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        const data = await response.json()
+
+        let translatedText = data.translatedText
+
     // add word to cloud
-    addWordToCloud(word);
+    addWordToCloud(word, translatedText);
     this.reset();
+
+    } catch (error) {
+        console.error('somethign fucked up during translation', error);
+        // translatedTextElement.textContent = 'Translation failed.';
+    }
 });
 
 function getRandomColor() {
@@ -19,14 +42,22 @@ function getRandomSize() {
 }
 
 
-function addWordToCloud(word) {
+function addWordToCloud(word, tT) {
     // This function would add the word to the visual word cloud
     // For the purposes of this example, we'll just append it to the div
     var wordCloud = document.getElementById('wordCloudBoard');
     var newWordSpan = document.createElement('span');
+
+    var newWordDefSpan = document.createElement('span');
+    
     newWordSpan.style.color = getRandomColor();
     newWordSpan.style.fontSize = `${getRandomSize()}px`;
     newWordSpan.textContent = word + " "; // Add space after word
+
+    newWordDefSpan.textContent = tT
+
     wordCloud.appendChild(newWordSpan);
+    wordCloud.appendChild(newWordDefSpan);
+    
   }
   
